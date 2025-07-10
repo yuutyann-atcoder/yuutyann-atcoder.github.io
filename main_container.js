02bb5ecd4f79d732093e7df786a169d4606eabc1
yuutyann-atcoder <140901587+yuutyann-atcoder@users.noreply.github.com>
2025-07-10 21:53:11 +0900

    Update and rename main_container.js to tech_class_main_container.js

--- main_container.js
+++
@@ -1,1 +0,0 @@
-globalThis.main_container = {"header" : "https://yuutyann-atcoder.github.io/header.js/", "child_containers" : []};
---
+++ tech_class_main_container.js
@@ -0,0 +1,1 @@
+globalThis.main_container = {"header" : "https://yuutyann-atcoder.github.io/tech_class_header.js/", "child_containers" : []};
