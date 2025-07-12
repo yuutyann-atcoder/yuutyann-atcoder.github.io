55205b3836d98320d6ae7dc8ff61c94ecbb4fc0e
yuutyann-atcoder <140901587+yuutyann-atcoder@users.noreply.github.com>
2025-07-12 17:15:30 +0900

    Update tech_class_main_container.js

--- tech_class_main_container.js
+++ tech_class_main_container.js
@@ -1,1 +1,1 @@
-callback_main_container({"header" : "https://yuutyann-atcoder.github.io/tech_class_header.js/", "child_containers" : []});
+callback_main_container({"header" : "https://yuutyann-atcoder.github.io/tech_class_header.js/", "child_containers" : [], "experimental_features" : []});
